b0c4590a5375c6d3ebd81eb490b51a24a7ee9112
NimendraK <nimendra.2019264@iit.ac.lk>
2020-10-01 13:13:26 +0530

    Junit Added

---
+++ src/main/java/it/albertotn/mongodb/util/DocumentUtilTest.java
@@ -0,0 +1,273 @@
+package it.albertotn.mongodb.util;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ToStringExclude;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
+import com.mongodb.util.JSON;
+
+import static it.albertotn.mongodb.util.DocumentUtil.recursiveNestedCreator;
+
+class DocumentUtilTest {
+
+    private static final Logger log = LoggerFactory.getLogger(DocumentUtil.class);
+
+    private static final Object ID = "_id";
+
+    @Test
+    public static List<String> convertToString(final List<Document> result) {
+        if (result == null) {
+            return new ArrayList<>();
+        }
+        List<String> res = new ArrayList<>();
+        result.stream().forEachOrdered(r -> res.add(r.toJson()));
+        return res;
+    }
+
+    @Test
+    public static List<ObjectId> getDocumentIds(final List<Document> mainDocuments) {
+        if (mainDocuments == null) {
+            return new ArrayList<>();
+        }
+        return mainDocuments.stream().map(d -> d.getObjectId(ID)).collect(Collectors.toList());
+    }
+
+    @Test
+    public static Object apply(final Document document, final String jsonPath) {
+        return apply(document, jsonPath, false);
+    }
+
+    @Test
+    public static Object apply(final Document document, final String jsonPath, final boolean printLog) {
+        if (document == null || jsonPath == null) {
+            return null;
+        }
+        final String json = JSON.serialize(document);
+        try {
+            return JsonPath.read(json, jsonPath);
+        } catch (PathNotFoundException pnf) {
+            if (printLog) {
+                log.error("Error during jsonpath read", pnf);
+            }
+            return null;
+        }
+    }
+
+    @Test
+    public static boolean isEmpty(Bson bson) {
+        if (bson == null) {
+            return true;
+        }
+        return ((Document) bson).toJson().equals("{ }");
+    }
+
+    @Test
+    public static Document unArray(final Document document) {
+        if (document == null) {
+            return null;
+        }
+        Document result = new Document();
+        for (Map.Entry<String, Object> entry : document.entrySet()) {
+            if (isBase(entry.getValue())) {
+                result.append(entry.getKey(), entry.getValue());
+            } else if (entry.getValue() instanceof List) {
+                List<?> val = (List<?>) entry.getValue();
+                if (val != null && !val.isEmpty() && val.size() == 1 && isBase(val.get(0))) {
+                    result.append(entry.getKey(), val.get(0));
+                } else {
+                    result.append(entry.getKey(), internalUnArray(val));
+                }
+            } else if (entry.getValue() instanceof Document) {
+                result.append(entry.getKey(), unArray((Document) entry.getValue()));
+            }
+
+        }
+        return result;
+    }
+
+    @Test
+    private static Document internalUnArray(List<?> values) {
+        if (values == null) {
+            return null;
+        }
+        Object current = values.get(0);
+        if (current instanceof Document) {
+            return unArray((Document) current);
+        } else if (values.get(0) instanceof List) {
+            return unArray(((List<Document>) current).get(0));
+        }
+        return new Document(values.get(0).toString(), values.get(1));
+    }
+
+    @Test
+    private static boolean isBase(Object value) {
+        if (value == null) {
+            return false;
+        }
+        return value instanceof String || value instanceof Long || value instanceof Integer
+                || value instanceof Date /* || value instanceof ObjectId */;
+    }
+
+    @Test
+    public static String escapeEscape(String value) {
+        if (value == null || value.isEmpty()) {
+            return value;
+        }
+        if (value.contains("\\")) {
+            return value.replace("\\\\", "\\");
+        } else if (value.contains("//")) {
+            return value.replace("//", "/");
+        }
+        return value;
+    }
+
+    @Test
+    public static Document nestedCreator(final String path, final Object value) {
+        if (StringUtils.isEmpty(path)) {
+            return null;
+        }
+        if (!path.contains(".")) {
+            return new Document(path, value);
+        }
+        String[] paths = StringUtils.split(path, ".");
+        return recursiveNestedCreator(paths, 0, value, new Document());
+    }
+
+    static Document recursiveNestedCreator(String[] paths, int i, Object value, Document current) {
+        if (paths == null) {
+            return current;
+        }
+        if (i < paths.length - 1) {
+            current.append(paths[i], recursiveNestedCreator(paths, ++i, value, new Document()));
+        } else if (i == paths.length - 1) {
+            current.append(paths[i], value);
+        }
+        return current;
+    }
+
+    @Test
+    public static Document union(final Document first, final Document second) {
+        if (first == null || second == null) {
+            return null;
+        }
+        Document result = new Document();
+        if (first.isEmpty()) {
+            return second;
+        }
+        if (second.isEmpty()) {
+            return first;
+        }
+        // first see if there are matching keys between first and second
+        for (Map.Entry<String, Object> entry : first.entrySet()) {
+            if (second.containsKey(entry.getKey())) {
+                Object f = first.get(entry.getKey());
+                Object s = second.get(entry.getKey());
+                if (f instanceof Document && s instanceof Document) {
+                    Document unionDoc = union((Document) f, (Document) s);
+                    result.append(entry.getKey(), unionDoc);
+                }
+            } else {
+                result.append(entry.getKey(), entry.getValue());
+            }
+        }
+        for (Map.Entry<String, Object> entry : second.entrySet()) {
+            if (!result.containsKey(entry.getKey())) {
+                result.append(entry.getKey(), entry.getValue());
+            }
+        }
+        return result;
+    }
+
+    @Test
+    public static Document copyRemoveFirstLevelField(final Document doc, final String field) {
+        if (doc == null || field == null) {
+            return null;
+        }
+        Document res = new Document();
+        for (String key : doc.keySet()) {
+            if (!key.equals(field)) {
+                res.append(key, doc.get(key));
+            }
+        }
+        return res;
+    }
+
+    @Test
+    public static Document nestedAttributeCreator(final Map<String, Object> doc) {
+        if (doc == null) {
+            return null;
+        }
+        if (doc.keySet().isEmpty()) {
+            return new Document(doc);
+        }
+        Document res = new Document();
+        for (String key : doc.keySet()) {
+            if (doc.get(key) instanceof ArrayList) {
+                List<?> list = (ArrayList<?>) doc.get(key);
+                if (list.isEmpty()) {
+                    res = union(res, new Document(key, doc.get(key)));
+                }
+                Document innerDoc = new Document();
+                for (Object elem : list) {
+                    if (elem instanceof Map) {
+                        innerDoc = union(res, nestedAttributeCreator((Map<String, Object>) elem));
+                    } else if (elem instanceof String) {
+                        // test only
+                    }
+                }
+                if (!innerDoc.isEmpty()) {
+                    ArrayList<Document> tmp = new ArrayList<>();
+                    tmp.add(innerDoc);
+                    res = union(res, new Document(key, tmp));
+                }
+            } else {
+                if (!key.contains(".")) {
+                    res.append(key, doc.get(key));
+                } else {
+                    Document tmp = nestedCreator(key, doc.get(key));
+                    String subKey = tmp.keySet().iterator().next();
+                    res = union(res, new Document(subKey, tmp.get(subKey)));
+                }
+            }
+        }
+        return res;
+    }
+
+    @Test
+    public static Document findByAttribute(Document doc, String key, String value) {
+        if (doc == null || key == null || value == null) {
+            return null;
+        }
+        if (doc.containsKey(key)) {
+            Object val = doc.get(key);
+            if (val instanceof String && ((String) val).equals(value)) {
+                return doc;
+            }
+        }
+        Document res = null;
+        for (String k : doc.keySet()) {
+            Object val = doc.get(k);
+            if (val instanceof Document) {
+                res = findByAttribute((Document) val, key, value);
+                if (res != null) {
+                    return res;
+                }
+            }
+        }
+        return null;
+    }
+}
